072a40b5767d3c203227dc4dafd7d877ab2f62d0
HofeSa20190213 <40146720190213@litec.ac.at>
2024-02-28 00:47:39 +0100

    feat: added a table for url display

--- URL-Short-Java/frontend/url-shortener/src/components/UrlTable.tsx
+++ URL-Short-Java/frontend/url-shortener/src/components/UrlTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 export type URLEntry = {
   originalUrl: string;
@@ -7,6 +7,7 @@ export type URLEntry = {
 };
 
 const UrlTable = () => {
+  const [urlEntries, setUrlEntries] = useState<URLEntry[]>([]);
   const titles = ["Original URL", "Shortened URL", "Clicks"];
   const titleListItems = titles.map((title) => (
     <th
@@ -18,14 +19,15 @@ const UrlTable = () => {
     </th>
   ));
 
-  const urlEntries: URLEntry[] = [];
-  const urlEntriesList = urlEntries.map((urlEntry) => {
-    <tr>
+  const addNewUrlEntry = () => {};
+
+  const urlEntriesListItems = urlEntries.map((urlEntry) => (
+    <tr key={urlEntry.originalUrl}>
       <td className="px-6 py-4 whitespace-nowrap">{urlEntry.originalUrl}</td>
       <td className="px-6 py-4 whitespace-nowrap">{urlEntry.shortendUrl}</td>
       <td className="px-6 py-4 whitespace-nowrap">{urlEntry.clicks}</td>
-    </tr>;
-  });
+    </tr>
+  ));
 
   return (
     <>
@@ -34,18 +36,7 @@ const UrlTable = () => {
           <tr>{titleListItems}</tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">Indiana</td>
-            <td className="px-6 py-4 whitespace-nowrap">Indianapolis</td>
-          </tr>
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">Ohio</td>
-            <td className="px-6 py-4 whitespace-nowrap">Columbus</td>
-          </tr>
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">Michigan</td>
-            <td className="px-6 py-4 whitespace-nowrap">Detroit</td>
-          </tr>
+          {urlEntriesListItems}
         </tbody>
       </table>
     </>
